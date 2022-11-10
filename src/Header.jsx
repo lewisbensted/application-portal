ada2e3e2a50249f5ef3eacb3524c05b4402fcd46
Lewis Bensted <lewisbensted@hotmail.com>
2022-11-10 16:51:01 +0000

    project setup

--- src/Header.jsx
+++ src/Header.jsx
@@ -5,7 +5,6 @@ import styles from "./Header.module.css";
 const Header = () => {
   return (
     <div className={styles.Header}>
-      <LogoSvg className={styles.logo} />
       <h1>Application Portal</h1>
     </div>
   );
