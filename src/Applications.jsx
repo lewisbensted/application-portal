4d08a20d7691a4e6eed4dca0316b70891e8a47a4
Lewis Bensted <lewisbensted@hotmail.com>
2022-11-11 18:43:11 +0000

    added typecript config

--- src/Applications.jsx
+++ src/Applications.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import SingleApplication from "./SingleApplication";
-import { getSingleApplicationFixture } from "./__fixtures__/applications.fixture";
+import { getApplicationsFixture } from "./__fixtures__/applications.fixture";
 import styles from "./Applications.module.css";
 
 const Applications = () => {
-  const applications = getSingleApplicationFixture;
+  const applications = getApplicationsFixture;
   return (
     <div className={styles.Applications}>
       <SingleApplication application={applications[0]} />
---
+++ src/__fixtures__/applications.fixture.ts
@@ -0,0 +1,23 @@
+export interface Applicant {
+  guid: string,
+  loan_amount: number,
+  first_name: string,
+  last_name: string,
+  company: string,
+  email: string,
+  date_created: string,
+  expiry_date: string,
+}
+
+export const getApplicationsFixture: Applicant[] = [
+  {
+    guid: "8a8f6cbc-77a1-4086-8968-a57816f4ff60",
+    loan_amount: 37597,
+    first_name: "Nikita",
+    last_name: "Kruschev",
+    company: "Communist Party of the Soviet Union",
+    email: "thegeneralsecretary@CPSU.com",
+    date_created: "2021-08-10",
+    expiry_date: "2021-12-02",
+  },
+];
